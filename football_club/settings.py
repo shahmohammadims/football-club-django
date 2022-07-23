5b909f1bf02cb308557294a98544c78b057005cb
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-07-23 18:29:24 +0430

    update templates

--- football_club/settings.py
+++ football_club/settings.py
@@ -32,8 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'adminlte3',
-    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR , 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
