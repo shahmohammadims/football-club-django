ad08b6fdc94f9d9154a417ae739039a28e2ae594
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-07-23 17:14:28 +0430

    custom user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin , BaseUserManager
 
-# Create your models here.
+class AccountManager(BaseUserManager):
+    def create_user(self , phone_number , first_name , last_name , password=None):
+        if not phone_number or not first_name or not last_name:
+            raise ValueError('account must have everything (national_code , phone_number , first_name , last_name , sex)')
+        user = self.model(
+            phone_number = phone_number,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self , phone_number , first_name , last_name , password=None):
+        user = self.create_user(
+            phone_number = phone_number,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+        
+class Account(AbstractBaseUser , PermissionsMixin):
+    phone_number = models.IntegerField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+    objects = AccountManager()
+    
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['first_name','last_name']
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['-id']
--- football_club/settings.py
+++ football_club/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'adminlte3',
-    'adminlte3-theme',
+    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +127,5 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.Account'
