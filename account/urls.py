f2edca20aba5c5a085420377d915d64e92a0aeee
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-07-27 03:49:50 +0430

    fixed bug

--- account/urls.py
+++ account/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('' , TemplateView.as_view(template_name='home.html') , name='home'),
     path('profile/' , views.ProfileView.as_view() , name='profile'),
     path('login/' , LoginView.as_view(redirect_authenticated_user=True,next_page='account:profile'),name='login'),
-    path('logout/' , LogoutView.as_view() , name='logout'),
+    path('logout/' , LogoutView.as_view(next_page='/') , name='logout'),
     path('change-password/' , PasswordChangeView.as_view(success_url='account:profile',template_name='account/password_change_form.html') , name='change_password')
 ]
