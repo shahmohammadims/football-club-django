8d8c58f0737a4c942bdb7b48f10d5b0b9f097ccf
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-07-30 21:30:18 +0430

    remove sqlite and start postgresql

--- football_club/settings.py
+++ football_club/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'football_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'football_club',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
