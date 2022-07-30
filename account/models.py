77574edb33db2ca40a8639af448e1cebda8126fc
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-07-30 21:54:35 +0430

    delete account manager and update phone number

--- account/models.py
+++ account/models.py
@@ -2,42 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin , BaseUserManager
 from payment.models import Payment
 from exercise.models import Exercise
-from django.core.validators import RegexValidator
 
-class AccountManager(BaseUserManager):
-    def create_user(self , phone_number , first_name , last_name , password=None):
-        if not phone_number or not first_name or not last_name:
-            raise ValueError('account must have everything (national_code , phone_number , first_name , last_name , sex)')
-        user = self.model(
-            phone_number = phone_number,
-            first_name = first_name,
-            last_name = last_name,
-        )
-        user.set_password(password)
-        user.save()
-        return user
-    
-    def create_superuser(self , phone_number , first_name , last_name , password=None):
-        user = self.create_user(
-            phone_number = phone_number,
-            first_name = first_name,
-            last_name = last_name,
-        )
-        user.set_password(password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.view_profile = False
-        user.save()
-        return user
-        
 class Account(AbstractBaseUser , PermissionsMixin):
-    pattern = RegexValidator(r'[9][0-9]{9}')
-    phone_number = models.IntegerField(unique=True , validators=[pattern])
+    phone_number = models.CharField(unique=True , max_length=11)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    objects = AccountManager()
+    objects = BaseUserManager()
     
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS = ['first_name','last_name']
