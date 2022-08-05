e8233b802c403b6b733105ed0ef9e5b4c3869c27
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-05 17:22:12 +0430

    setup for start api payment

--- app/payment/urls.py
+++ app/payment/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
+from .api.urls import urlpatterns
 
 app_name = 'payment'
 
 urlpatterns = [
-    path('' , views.IndexView.as_view(), name='index')
+    path('' , views.IndexView.as_view(), name='index'),
+    path('api/', include(urlpatterns)),
 ]
