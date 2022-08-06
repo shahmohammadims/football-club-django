4eeb59af83ad08156124af8ede07e2170eb17003
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-06 23:19:19 +0430

    setup for start api account

--- app/account/urls.py
+++ app/account/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.views import LoginView , LogoutView , PasswordChangeView
 from django.views.generic import TemplateView
 from . import views
+from .api.urls import urlpatterns
+
 app_name = 'account'
 
 urlpatterns = [
@@ -9,5 +11,6 @@ urlpatterns = [
     path('profile/' , views.ProfileView.as_view() , name='profile'),
     path('login/' , LoginView.as_view(redirect_authenticated_user=True,next_page='account:profile'),name='login'),
     path('logout/' , LogoutView.as_view(next_page='/') , name='logout'),
-    path('change-password/' , PasswordChangeView.as_view(success_url='account:profile',template_name='account/password_change_form.html') , name='change_password')
+    path('change-password/' , PasswordChangeView.as_view(success_url='account:profile',template_name='account/password_change_form.html') , name='change_password'),
+    path('api/', include(urlpatterns))
 ]
