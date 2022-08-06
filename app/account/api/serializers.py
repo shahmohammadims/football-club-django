62338b3feb054413c25f5efc52fd62130f238404
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-06 23:30:54 +0430

    add group for account serializer

--- app/account/api/serializers.py
+++ app/account/api/serializers.py
@@ -4,4 +4,4 @@ from ..models import Account
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['phone_number', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'debt', 'exercises_not_payment']
+        fields = ['phone_number', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'debt', 'exercises_not_payment', 'groups']
