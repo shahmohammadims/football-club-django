be283f513e73052c9ba91a4c356331aa5d83099d
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-06 23:40:35 +0430

    create get for account detail in api

--- app/account/api/urls.py
+++ app/account/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.AccountList.as_view())
+    path('', views.AccountList.as_view()),
+    path('<pk>', views.AccountDetail.as_view()),
 ]
--- app/account/api/views.py
+++ app/account/api/views.py
@@ -1,6 +1,22 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import Http404
 from .serializers import AccountSerializer
 from ..models import Account
 class AccountList(ListAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+
+class AccountDetail(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return Account.objects.get(phone_number=pk)
+        except:
+            raise Http404
+    
+    def get(self, request, pk):
+        instance = self.get_object(pk)
+        serializer = AccountSerializer(instance,many=False)
+        return Response(serializer.data)
