56f0dad9b1ce8779cd974a6fb105ccccda008ba9
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-07 18:55:52 +0430

    add get for category detail in api

--- app/exercise/api/views.py
+++ app/exercise/api/views.py
@@ -1,7 +1,23 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from ..models import Category, Exercise
 from .serializers import CategorySerializer, ExerciseSerializer
+from django.http import Http404
 
 class CategoryList(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+class CategoryDetail(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except:
+            raise Http404
+    
+    def get(self, request, pk):
+        instance = self.get_object(pk)
+        serializer = CategorySerializer(instance, many=False)
+        return Response(serializer.data)
