92bb7e2b212ab6ac02ba1eca9571ee677d688ae3
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-07 18:03:55 +0430

    add accountforpayment for show full_name in api

--- app/payment/api/serializers.py
+++ app/payment/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from ..models import Payment
+from account.models import Account
 
+class AccountForPayment(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['full_name']
 class PaymentSerializer(serializers.ModelSerializer):
+    account = AccountForPayment()
     class Meta:
         model = Payment
-        fields = '__all__'
+        fields = ['account','price','items']
+        
